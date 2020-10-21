2cbe40689b17e6496c0f3fd85a6204b5540433c0
Daniel Braas <danielbraas@gmail.com>
2020-10-21 06:16:31 -0500

    Created slider to set number of bubbles

--- sketch.js
+++ sketch.js
@@ -1,5 +1,5 @@
 let bubbles = [];
-let n;
+let n = 20;
 
 function setup() {
   canvas = createCanvas(600, 600);
@@ -12,7 +12,7 @@ function setup() {
     let RGB = [random(255), random(255), random(255)];
     bubbles[i] = new Bubble(x, y, rad, RGB);
   }
-  n = createSlider(1, 100, 20)  
+  n = createSlider(1, 100, 20).value()  
   p = createP('This is another paragraph created in JS')
 }
 
